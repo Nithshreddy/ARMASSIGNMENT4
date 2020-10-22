3950201527a7f9b8984e9bc6ff6d9a9838cee2e7
Nithshreddy <33860430+Nithshreddy@users.noreply.github.com>
2020-10-23 00:29:38 +0530

    Update LOG.s

--- LOG.s
+++ LOG.s
@@ -1,26 +1,27 @@
-AREA log,CODE,READONLY
+AREA appcode,CODE,READONLY
     EXPORT __main
 		ENTRY 
+		;main logic here is looping the ith term (-1)^(i+1) * x^i /i
+		;log(1+x) =summation of((-1)^(i+1) * x^i /i) from i =0 to i = n
 __main  FUNCTION
-          MOV R0,#100;(i value)
-          MOV R1,#0
-          VLDR.F32 S0,=0;Holding the sum of series elements 's'
-          VLDR.F32 S1,=0.75;Temp Variable to hold the intermediate series elements 't'
-          VLDR.F32 S2,=0.75;Holding 'x' Value(to converge x<1)
-          VLDR.F32 S3,=1;addition faactor
+          MOV R1,#10;n value (no of iterations)
+          MOV R0,#0 ;i value (present iteration)
+          VLDR.F32 S0,=0.25;sum of all the terms till its iteration
+          VLDR.F32 S1,=0.25;Ti
+          VLDR.F32 S2,=0.25;x value(-1<x<1)
+          VLDR.F32 S3,=1;for incrementing
           VLDR.F32 S4,=-1;multiplication factor
-          VLDR.F32 S5,=0 ;i value (i=0)
-exp      CMP R1,R0;Compare 'i' and 'n'
-          BLE exp1;if i < n goto LOOP
-          B stop;else goto stop
+          VLDR.F32 S5,=1 ;i value (i=0)
+exp       CMP R1,R0;Compare 'n' and 'i'
+          BLE exp1;if i < n goto exp1
+          B STOP;else goto stop
 exp1      VMUL.F32 S1,S1,S2;t = t*x
           VMUL.F32 S1,S1,S4;t=-1*t*x
-          VADD.F32 S6,S5,S3;i=i+1
-          VDIV.F32 S7,S6,S5;k=i/i+1
-          VFMA.F32 S0,S1,S7;sum=sum+(-t*x*i)/(i+1)
           VADD.F32 S5,S5,S3;i=i+1
-          SUB R0,#1
+          VDIV.F32 S7,S3,S5;k=1/i+1
+          VFMA.F32 S0,S1,S7;sum=sum+(-t*x*i)/(i+1)
+          ADD R0,#1 ;adding i after every iteration
           B exp
-stop    B stop                          ; stop program
+STOP      B STOP  ; stop program
      ENDFUNC
 	 END
